cbfc9180f3c38cc7505feb58c1abab30a11021ac
Deathlimit <danilreshetka@gmail.com>
2024-11-15 18:59:51 +0300

    styled with Mozilla

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 
-using namespace std;
-
-int main(){
+int 
+main(){
 
 //Cool comment
 //123
 //test
+
 string name;
-cin >> name;
-cout << "Hello World, " << name;
+std::cin >> name;
+std::cout << "Hello World, " << name;
 
 return 0;
-
 }
